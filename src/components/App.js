504e0ccd4e200fd4fe7d11b4f34f78072104ead4
pratik2820 <pratikmorge28@gmail.com>
2023-10-06 12:02:35 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import ChildComponent from "./ChildComponent";
 
 const App = () => {
+
+  const [showModal,setShowModal] = useState(false);
+
+
+  function handleClick(){
+    setShowModal(true);
+  }
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="parent">
+
+          <h1>Parent Component</h1>
+          <ChildComponent showModal={showModal} handleClick={handleClick}/>
+
+
+        </div>
     </div>
   )
 }
---
+++ src/components/ChildComponent.js
@@ -0,0 +1,27 @@
+
+import React from "react";
+
+
+const ChildComponent = ({showModal,handleClick}) =>{
+
+
+    return(
+        <div className="child">
+            <h2>Child Component</h2>
+            <button onClick={handleClick}>Show Modal</button>
+            {
+                showModal && (
+                    <div>
+                        <h3>Modal Content</h3>
+                        <p>This is the Modal Content</p>
+                    </div>
+                )
+            }
+
+
+
+        </div>
+    )
+}
+
+export default ChildComponent;
